8c0b65de602369aa4871051574424743043dcbab
darrenyhc-jpg <darrenyhc@gmail.com>
2026-07-24 13:10:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,30 +10,33 @@ document.querySelectorAll("nav a").forEach(link => {
 
     });
 });
-<script>
-    const images = document.querySelectorAll("img");
-
-    const lightbox = document.createElement("div");
-    lightbox.className = "lightbox";
-
-    const lightboxImage = document.createElement("img");
-    lightbox.appendChild(lightboxImage);
-
-    document.body.appendChild(lightbox);
-
-    images.forEach(image => {
-        image.addEventListener("click", () => {
-
-            lightboxImage.src = image.src;
-            lightbox.classList.add("active");
-
-        });
-    });
 
 
-    lightbox.addEventListener("click", () => {
+const images = document.querySelectorAll("img");
 
-        lightbox.classList.remove("active");
+const lightbox = document.createElement("div");
+lightbox.className = "lightbox";
+
+const lightboxImage = document.createElement("img");
+lightbox.appendChild(lightboxImage);
+
+document.body.appendChild(lightbox);
+
+
+images.forEach(image => {
+
+    image.addEventListener("click", () => {
+
+        lightboxImage.src = image.src;
+        lightbox.classList.add("active");
 
     });
-</script>
+
+});
+
+
+lightbox.addEventListener("click", () => {
+
+    lightbox.classList.remove("active");
+
+});
