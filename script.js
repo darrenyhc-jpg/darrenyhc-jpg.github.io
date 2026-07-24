2ddac7f5723fb6930874168c45cbbdb9224ead50
darrenyhc-jpg <darrenyhc@gmail.com>
2026-07-24 13:00:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ document.querySelectorAll("nav a").forEach(link => {
 
     });
 });
+<script>
+    const images = document.querySelectorAll("img");
+
+    const lightbox = document.createElement("div");
+    lightbox.className = "lightbox";
+
+    const lightboxImage = document.createElement("img");
+    lightbox.appendChild(lightboxImage);
+
+    document.body.appendChild(lightbox);
+
+    images.forEach(image => {
+        image.addEventListener("click", () => {
+
+            lightboxImage.src = image.src;
+            lightbox.classList.add("active");
+
+        });
+    });
+
+
+    lightbox.addEventListener("click", () => {
+
+        lightbox.classList.remove("active");
+
+    });
+</script>
